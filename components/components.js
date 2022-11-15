26aae2eac35223d8e2e6cf94ff6cd423c36e4114
Revathi Mahalingam <103559154+revathimsc@users.noreply.github.com>
2022-11-15 20:05:32 +0530

    Add files via upload

---
+++ components/components.js
@@ -0,0 +1,29 @@
+class NavbarComponent extends HTMLElement {
+  connectedCallback() {
+    
+      const logoIconContainer='<div id="logo-icon-container"><div id="logo"><a href="index.html"><h1>IMDb</h1></a></div></div>';
+      const searchContainer='<div id="search-container"><input id="search-bar" placeholder="search movie by name" /></div>';
+      const  WatchlaterContainer  ='<div id="fav-icon-container"><div id="fav"><a href="watchLater.html"><span> <i class="fa-solid fa-bookmark"></i> <h4>Watchlist</h4></span></a></div></div>'
+      
+      const nav =`<nav><div class="nav-container">${logoIconContainer}${searchContainer}${WatchlaterContainer}</div></nav>`;
+
+      const searchResult ='<div id="search-result-outer-container"><ul id="search-result-container"></ul></div>';
+      
+      this.innerHTML = nav + searchResult;
+  }
+}
+customElements.define("navbar-component", NavbarComponent);
+
+class FooterComponent extends HTMLElement{
+  connectedCallback(){
+    this.innerHTML=`<footer>
+    <a href="https://www.linkedin.com/in/sriram-a-78494b1aa/" target="_blank"><p>Linkedin</p></a>
+    <a href="https://github.com/sriramarumugham/IMDb_Clone_JavaScript" target="_blank"><p>GitHub</p></a>
+    <a href="https://www.instagram.com/itz_xxiii/?hl=en" target="_blank"><p>Instagram</p></a>
+    <a href="https://www.youtube.com/channel/UCgadvIAHMlBB-FEFApnOKZA" target="_blank"><p>Youtube</p></a>
+    </footer>`
+  }
+}
+customElements.define('footer-component' , FooterComponent);
+
+
---
+++ script/script.js
@@ -0,0 +1,114 @@
+const searchBar = document.getElementById("search-bar");
+
+const searchResultContainer = document.getElementById(
+  "search-result-container"
+);
+
+
+//to hide the search result continer when clicking  anywhere other than the search container in hte page
+document.addEventListener("click", function (event) {
+  if (searchResultContainer && !searchResultContainer.contains(event.target)) {
+    searchResultContainer.classList.add("hide-search-container");
+  }
+});
+
+//on typing in the input container
+searchBar.addEventListener("keyup", () => {
+  const input = searchBar.value;
+
+  //if there length of the input is greate than 0
+  if (input.length > 0) {
+
+    //show the auto suggestion container
+    searchResultContainer.classList.remove("hide-search-container");
+
+    //fill the html markup of th container
+    searchResultContainer.innerHTML = `<li id="search-result-movie-card"> 
+    <div id="search-result-img-container">
+      <a><img
+        src="${"https://cdn-icons-png.flaticon.com/512/7797/7797065.png"}"
+      />
+      </a>
+    </div>
+    <div id="search-result-movie-title">
+      <h4>${input}</h4>
+    </div>
+   </li>`;
+
+   //fetch the api request with the search input
+    debounce(input);
+  }
+});
+
+//global variable interval;
+
+let interval = 0;
+
+//debounce function delays the api request by 0.350 seconds
+const debounce = (input) => {
+  
+  
+
+  // deletes the previous request before creating a new request
+  clearTimeout(interval);
+  
+  interval = setTimeout(() => {
+   
+    // clearTimeout(interval);
+    
+    //new api request
+    getMovies(input);
+
+  }, 350);
+
+};
+
+// let apiKey = "k_dbmklrp6";
+let apiKey = "k_o7kcaag9";
+// let apiKey="k_8073znj9";
+
+let rootUrl = "https://imdb-api.com/en/API/Search/";
+
+const getMovies = async (serachTerm) => {
+
+  let url = `${rootUrl}${apiKey}/${serachTerm}`;
+
+  const response = await fetch(url);
+
+  response.json().then((data) => {fillHTML(data);});
+};
+
+const fillHTML = (movie) => {
+  searchResultContainer.innerHTML = "";
+  const movies = movie.results;
+
+  //unabel to find the move
+  if (!movies) {
+    searchResultContainer.innerHTML = ` 
+    <li>
+      <div id="not-found-container">
+      <h1>Not found!</h1>
+      </div>
+    </li>`;
+  } 
+ 
+  //found the movie
+  else {
+    searchResultContainer.innerHTML = "";
+    movies.filter((movie) => {
+      searchResultContainer.innerHTML += `  
+      <li id="search-result-movie-card"> 
+         <div id="search-result-img-container">
+           <a href="detail.html?id=${movie.id}">
+           <img
+             src="${movie.image? movie.image: "https://cdn-icons-png.flaticon.com/512/7797/7797065.png"}
+           "/>
+           </a>
+         </div>
+         <div id="search-result-movie-title">
+          <a href="detail.html?id=${movie.id}"> <h4>${movie.title}</h4> </a>
+         </div>
+      </li>`;
+    });
+  }
+};
